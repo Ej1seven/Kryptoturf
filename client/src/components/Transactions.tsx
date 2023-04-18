594fd471ff4f80831f3d7fc1d6d61526a6600f19
Ej1seven <erikhunter@erikhunter.dev>
2023-04-17 20:32:34 -0500

    Krytoturf beta

--- client/src/components/Transactions.tsx
+++ client/src/components/Transactions.tsx
@@ -27,7 +27,7 @@ const TransactionCard: React.FC<TransactionCardProps> = ({
     be returned in url format. */
   const gifUrl = useFetch({ keyword });
   return (
-    <div className="bg-[#181918] m-4 flex flex-1 2xl:min-w-[450px] 2xl:max-w-[500px] sm:min-w-[270px] sm:max-w-[300px] flex-col p-3 rounded-md hover:shadow-2xl">
+    <div className="bg-[#180c1a] m-4 flex flex-1 2xl:min-w-[450px] 2xl:max-w-[500px] sm:min-w-[270px] sm:max-w-[300px] flex-col p-3 rounded-md hover:shadow-2xl">
       <div className="flex flex-col items-center w-full mt-3">
         <div className="display-flex justify-start w-full mb-6 p-2">
           <a
